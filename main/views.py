2d282c9e819b909338bbfd6d55a00da40da3fd1a
Студент NIS <Student@akb.nis.edu.kz>
2024-11-09 23:34:33 +0500

    modified models and render

--- main/views.py
+++ main/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Restroom
+from collections import defaultdict
+
 
 def hello_world(request):
     return HttpResponse("hello")
-def index(req):
-    return render(req,'main/index.html')
+def index(request):
+    # Retrieve all restrooms from the database
+    first_floor_restrooms = Restroom.objects.filter(floor=1)
+    second_floor_restrooms = Restroom.objects.filter(floor=2)
+    third_floor_restrooms = Restroom.objects.filter(floor=3)
+
+
+    return render(request, 'main/index.html', {
+            'first_floor_restrooms': first_floor_restrooms,
+            'second_floor_restrooms': second_floor_restrooms,
+            'third_floor_restrooms': third_floor_restrooms,
+        })
